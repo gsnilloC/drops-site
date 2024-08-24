4cf719fa8f861869e129e4aad47fabe43c6830a4
Collins Gichohi <cgichohi2018@gmail.com>
2024-08-23 22:48:45 -0700

    type writer is working

--- src/components/landing.js
+++ src/components/landing.js
@@ -1,9 +1,9 @@
-// src/components/landing.js
 import React, { useState } from "react";
+import Typewriter from "typewriter-effect";
 import SmsInstructions from "./sms";
 import EmailInstructions from "./email";
 import "../assets/styles/landing.css";
-import logo1 from "../assets/images/flightclub_logo.png"; // Adjust the paths as necessary
+import logo1 from "../assets/images/flightclub_logo.png";
 import logo2 from "../assets/images/goat_logo.png";
 import logo3 from "../assets/images/stockx_logo.png";
 
@@ -12,14 +12,31 @@ const Landing = () => {
 
   return (
     <div>
-      <p className="centered-paragraph">
-        Keep track of when the prices drop for the sneakers you want to cop
-      </p>
+      {/* Separate div for typewriter and toggles */}
+      <div className="header-container">
+        <div className="typewriter-container">
+          <Typewriter
+            options={{
+              strings: [
+                "Keep track of when the prices drop for the sneakers you want to cop",
+              ],
+              autoStart: true,
+              loop: false, // Disable looping so the text appears once and stays
+              delay: 30, // Faster typing speed
+              deleteSpeed: Infinity, // Set to Infinity to prevent deletion
+              cursor: "|", // Show cursor
+            }}
+          />
+        </div>
+      </div>
+
+      {/* Separate div for logos */}
       <div className="logo-container">
         <img src={logo1} alt="FlightClub" className="logo" />
         <img src={logo2} alt="Goat" className="logo" />
         <img src={logo3} alt="StockX" className="logo" />
       </div>
+
       <div className="toggle-container">
         <div
           className={`toggle-option ${showSmsInstructions ? "active" : ""}`}
