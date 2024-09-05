dbcd0d9e0cf4062b3e7ae026049013182f5acbbc
Collins Gichohi <cgichohi2018@gmail.com>
2024-09-04 19:07:48 -0700

    added contact links

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,16 +1,30 @@
 import React from "react";
-import { Button } from "@mui/material";
+import { Button, Menu, MenuItem } from "@mui/material";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import GitHubIcon from "@mui/icons-material/GitHub";
 import "../assets/styles/navbar.css";
 
 function Navbar() {
+  const [anchorEl, setAnchorEl] = React.useState(null);
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
   return (
     <nav className="navbar">
       <div className="navbar-left">
-        <a href="#home">drops.</a>
+        <a href=".home">drops.</a>
       </div>
       <div className="navbar-right">
         <Button
-          href="#contact"
+          aria-controls="simple-menu"
+          aria-haspopup="true"
+          onClick={handleClick}
           variant="contained"
           color="warning"
           sx={{
@@ -26,6 +40,36 @@ function Navbar() {
         >
           Contact
         </Button>
+        <Menu
+          id="simple-menu"
+          anchorEl={anchorEl}
+          keepMounted
+          open={Boolean(anchorEl)}
+          onClose={handleClose}
+        >
+          <MenuItem onClick={handleClose}>
+            <a
+              href="https://www.linkedin.com/in/collinsgichohi/"
+              target="_blank"
+              rel="noopener noreferrer"
+              style={{ display: "flex", alignItems: "center" }}
+            >
+              <LinkedInIcon sx={{ marginRight: "8px" }} />
+              LinkedIn
+            </a>
+          </MenuItem>
+          <MenuItem onClick={handleClose}>
+            <a
+              href="https://github.com/gsnilloc"
+              target="_blank"
+              rel="noopener noreferrer"
+              style={{ display: "flex", alignItems: "center" }}
+            >
+              <GitHubIcon sx={{ marginRight: "8px" }} />
+              GitHub
+            </a>
+          </MenuItem>
+        </Menu>
       </div>
     </nav>
   );
